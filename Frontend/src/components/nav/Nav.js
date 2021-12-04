0117eb71b24601d4e3610d414cab74c8b1c57d05
David Cao <davidcao@Davids-MBP-16.fios-router.home>
2021-12-04 17:41:26 -0500

    added logo to top left

--- Frontend/src/components/nav/Nav.js
+++ Frontend/src/components/nav/Nav.js
@@ -4,6 +4,7 @@ import styles from "./Nav.module.sass"
 import { setTheme } from '../../ducks/modules/UI'
 
 // import { Link } from 'react-router-dom'
+import { ReactComponent as Logo } from './FastKeysLogo.svg'
 
 export default function () {
     const dispatch = useDispatch()
@@ -19,7 +20,10 @@ export default function () {
             <ul className={styles.navLinks}>
                 {/* <Link to={'/'} target="_self"> */}
                 {/* </Link> */}
-                <li><a href="/">home</a></li>
+                <li><a href="/">
+                    {/* <img src={logo} alt="" className={styles.logo} /> */}
+                    <Logo className={styles.logo}/>
+                </a></li>
                 <li onClick={toggleTheme} className={`${styles.link} ${styles.lightSwitch}`}>{theme}</li>
             </ul>
         </nav>
