8fd2dab2462e0cdb2238884992af48c7ee16fd1c
David Cao <davidcao@Davids-MBP-16.fios-router.home>
2021-12-04 03:42:23 -0500

    added metric tracking to backend

--- Backend/app.py
+++ Backend/app.py
@@ -25,11 +25,22 @@ CORS(app)
 app.config["MONGO_URI"] = MONGO_URI
 mongo = PyMongo(app)
 gamesdb = mongo.db.games
+statsdb = mongo.db.stats
 
 # home route
 @app.route('/')
 def home():
     return 'hello'
+
+@app.route('/api/views', methods=['PUT'])
+def icr_views():
+    statsdb.update_one({}, {'$inc': {'views': 1}}, upsert=True)
+    return "", 200
+
+@app.route('/api/stats')
+def get_stats():
+    stats = statsdb.find_one({})
+    return json.dumps(stats, default=str)
 
 @app.route('/game', methods=['POST', 'DELETE'])
 def post_game():
@@ -49,12 +60,14 @@ def post_game():
             game_id = str(uuid.uuid4())[:8]
         doc['id'] = game_id
         gamesdb.insert_one(doc)
+        statsdb.update_one({}, {'$inc': {'games': 1}}, upsert=True)
         return jsonify(game_id)
 
 @app.route('/game/<game_id>', methods=['GET', 'DELETE'])
 def return_game(game_id=None):
     if request.method == 'GET':
         game = gamesdb.find_one({"id": game_id})
+        statsdb.update_one({}, {'$inc': {'game_played': 1}}, upsert=True)
         return json.dumps(game, default=str)
 
 if __name__ == '__main__':
