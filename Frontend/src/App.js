8286f16e690f772fa2e56f1d7a53d37d9f017ea5
David Cao <davidcao@Davids-MBP-16.fios-router.home>
2022-01-17 12:06:24 -0500

    track page views

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -13,7 +13,9 @@ import { BrowserRouter, Route, Switch, useParams } from 'react-router-dom'
 
 function App() {
     // track page views
-    axios.put(process.env.REACT_APP_BACKEND_URL + '/api/views')
+    React.useEffect(() => {
+        axios.put(process.env.REACT_APP_BACKEND_URL + '/api/views')
+    }, [])
 
     return (
         <Provider store={store}>
